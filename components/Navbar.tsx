e9b660e0c659ffe98fe2ed5679f3d2a276f47da4
mjmolieleng@gmail.com <mjmolieleng@gmail.com>
2021-11-04 07:48:31 +0200

    pivot to offer corporate cards

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,17 +1,20 @@
 import Head from "next/head";
 import Link from "next/link";
-
+import styles from "../styles/Navbar.module.css";
+// const style = StyleSheetList
 export default function Navbar() {
   return (
     <div>
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <div className="container">
           <Link href="/">
-            <a className="navbar-brand">Wiinc</a>
+            <a className="navbar-brand">
+              <strong>WiincCard</strong>
+            </a>
           </Link>
 
-          {/* <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav mx-auto mb-2 mb-lg-0">
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav mx-auto mr-auto mb-2 mb-lg-0">
               <li className="nav-item">
                 <Link href="/business">
                   <a className="nav-link active">For Business</a>
@@ -23,7 +26,7 @@ export default function Navbar() {
                 </Link>
               </li>
             </ul>
-          </div> */}
+          </div>
         </div>
       </nav>
     </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,17 +2,19 @@ import type { NextPage } from "next";
 import Image from "next/image";
 import Link from "next/dist/client/link";
 import Footer from "../components/Footer";
+import bankCard from "../public/wiincCard.svg";
 
 const Home: NextPage = () => {
   return (
     <div>
-      <section className="hero">
-        <div className="container ">
+      <section className="hero bg-dark text-light">
+        <div className="container px-4">
           <div className="row">
             <div className="col-md-12 py-5 text-center hero">
-              <h3>Global Cards for Africans</h3>
-              <p className="mt-5 lead">
-                Expanding the most used mobile payments usability in Africa
+              <h3 className="text-light">For Corporate Prepaid Cards</h3>
+              <p className="mt-5 lead text-light">
+                Issue prepaid debit cards employers for travelling, <br />
+                entertainment and online spending
               </p>
             </div>
           </div>
@@ -20,26 +22,45 @@ const Home: NextPage = () => {
           <div className="row">
             <div className="col-12 text-center">
               <Link href="business">
-                <a className="btn btn-primary">Request Access</a>
+                <a className="btn btn-primary">Get Wiinc Card</a>
               </Link>
             </div>
           </div>
         </div>
       </section>
 
-      <section className="container py-4">
+      <section className="container py-5">
+        <div className="row text-center my-5">
+          <h3>Why choose us</h3>
+        </div>
         <div className="row py-4 text-center">
           <div className="col-md-4 mb-3">
-            <h4 className="py-4 feature-item">Link</h4>
-            <p className="py-4">Link mobile money accounts</p>
+            <h4 className="py-4 feature-item">Travelling</h4>
+            <p className="p-4">
+              Book flights and make payments anywhere in the world
+            </p>
           </div>
           <div className="col-md-4 mb-3">
-            <h4 className="py-4 feature-item">Global</h4>
-            <p className="py-4">Enable global payments</p>
+            <h4 className="py-4 feature-item">Online</h4>
+            <p className="p-4">
+              Our cards are able to make online transactions
+            </p>
           </div>
           <div className="col-md-4 mb-3">
-            <h4 className="py-4 feature-item">Customize</h4>
-            <p className="py-4">Put your brand on the card</p>
+            <h4 className="p-4 feature-item">Customize</h4>
+            <p className="py-4">
+              You can request for branded cards for your brand
+            </p>
+          </div>
+        </div>
+      </section>
+
+      <section className="container-fluid bg-dark text-light text-center">
+        <div className="container py-4">
+          <div className="row">
+            <div className="col-12 py-4">
+              <h3>We have made it easy to track your employees spending</h3>
+            </div>
           </div>
         </div>
       </section>
